03cc7f947ab7232fbcc118cf5357e8f6c31ffa53
Hammed Tosin <hammedtosin188@gmail.com>
2022-06-30 04:23:04 -0700

    add test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if _name_ == "_main_":
-    from add_0 import add   
+    from add_0 import add
     a = 1
     b = 2
-print(f"{a} + {b} = {add(a, b)}")
+print(f"{a:d} + {b:d} = {add(a, b):d}")
