c828cf8381592675b0895feeb1af49d62430f5e2
Hammed Tosin <hammedtosin188@gmail.com>
2022-06-30 07:54:19 -0700

    fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculation_1 import add, mul, div, sub
+    from calculator_1 import add, mul, div, sub
     a = 10
     b = 5
     print(f"{a:d} + {b:d} = {add(a, b):d}")
