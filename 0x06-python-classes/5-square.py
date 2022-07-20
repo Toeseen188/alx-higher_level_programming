032002fa804298861a1a8f435537d3dcbbd2d545
Hammed Tosin <hammedtosin188@gmail.com>
2022-07-19 17:32:47 -0700

    fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -18,13 +18,13 @@ class Square:
 
     def my_print(self):
         """print #"""
-        if self.__size == 0:
-            print()
-        else:
+        if self.__size > 0:
             for i in range(self.__size):
                 for j in range(self.__size):
                     print("#", end='')
                 print()
+        else:
+            print()
 
     @property
     def size(self):
