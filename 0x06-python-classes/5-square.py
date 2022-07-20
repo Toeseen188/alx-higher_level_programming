616c820829296c2b4ac398dba8224ec4f361671f
Hammed Tosin <hammedtosin188@gmail.com>
2022-07-19 17:20:34 -0700

    OOP

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Documentation of class Square """
+
+
+class Square:
+    def __init__(self, size=0):
+        """initialise"""
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be > 0")
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+
+    @property
+    def size(self):
+        """return self.__size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """"setter method"""
+        self.__size = value
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be > 0")
