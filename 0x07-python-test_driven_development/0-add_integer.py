9e8f0072f73328dcb1f1a65e32cb132d6c080e29
Hammed Tosin <hammedtosin188@gmail.com>
2022-07-25 04:32:55 -0700

    first test task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" A module about a function that adds integer"""
+
+
+def add_integer(a, b=98):
+    """ A function that add
+    two integer or two float and raise TypeError
+    if they are not
+
+    Args:
+        a: must be int or float, raises a TypeError,
+            if not
+        b: must be int or float, raises a TypeError,
+            if not
+    Returns:
+        Returns the additon of a and b after casting
+        into an int
+
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+        return (a + b)
