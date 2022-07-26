3c2d1c3a3ad8cb68fd0c80d5b159ce0092d477f3
Hammed Tosin <hammedtosin188@gmail.com>
2022-07-26 13:34:28 -0700

    print_square tested

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module that print square with # """
+
+
+def print_square(size):
+    """ print # in size of a square
+    Args:
+        size(int): size of the square
+    Returns:
+        return # size of a square
+        """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(0, size):
+        for j in range(0, size):
+            print('#', end="")
+        print()
