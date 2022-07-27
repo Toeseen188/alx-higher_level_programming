c98c5a0b628748eef28474765e78d8e2b8520b07
Hammed Tosin <hammedtosin188@gmail.com>
2022-07-27 04:20:04 -0700

    class doc added

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,11 +3,13 @@
 
 
 class Square:
+    """ this class prints square with #
+    """
     def __init__(self, size=0):
         """initialise"""
         self.__size = size
 
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be > 0")
