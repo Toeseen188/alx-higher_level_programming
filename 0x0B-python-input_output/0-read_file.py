4245a8900c52efd27d313324f6a416157b1ba0e9
Hammed Tosin <hammedtosin188@gmail.com>
2022-08-02 08:12:58 -0700

    using read()

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This is a python fuction
+that read a file """
+
+
+def read_file(filename=""):
+    """ This function takes a filename(string)
+    as an argument, create a file with the name
+    and read the content stored in it
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+projects on python inputs and output, files
