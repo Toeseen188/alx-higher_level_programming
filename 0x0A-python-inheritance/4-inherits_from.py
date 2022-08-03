1396aef11773b5a4b2b4cc36977aa80052b16c14
Hammed Tosin <hammedtosin188@gmail.com>
2022-08-03 11:16:48 -0700

    issubclass used instead of isinstance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ if obj is a subclass of a_class"""
 def inherits_from(obj, a_class):
     """ Return True if obj is a subclass of a_class
     """
-    if isinstance(obj, a_class) and type(obj) is not a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         False
