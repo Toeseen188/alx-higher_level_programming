efd635970256c3f1f208a6965ba92a211ad80993
Hammed Tosin <hammedtosin188@gmail.com>
2022-08-10 07:29:42 -0700

    pycodestyling

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,6 +110,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """ making __str__ return string"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, 
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                 self.__x, self.__y, self.__width, self.__height))
-
