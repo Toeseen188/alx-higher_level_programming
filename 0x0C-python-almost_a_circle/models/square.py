526528a47e0d13441e4a17661d0d2e1b09116666
Hammed Tosin <hammedtosin188@gmail.com>
2022-08-12 15:27:22 -0700

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,29 @@ class Square(Rectangle):
         """ return string"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """assigns properties to a list of args"""
+        if (args):
+            if len(args) > 0:
+                for i in range(len(args)):
+                    if i == 0:
+                        self.id = args[0]
+                    if i == 1:
+                        self.size = args[1]
+                    if i == 2:
+                        self.x = args[2]
+                    if i == 3:
+                        self.y = args[3]
+
+        else:
+            if len(kwargs) > 0:
+                for key in kwargs.keys():
+                    if key == "id":
+                        self.id = kwargs["id"]
+                    if key == "size":
+                        self.size = kwargs["size"]
+                    if key == "x":
+                        self.x = kwargs["x"]
+                    if key == "y":
+                        self.y = kwargs["y"]
