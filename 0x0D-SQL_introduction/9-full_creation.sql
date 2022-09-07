841ae67cbb51d3a1596342c8fc57bce9b760b624
Hammed Tosin <hammedtosin188@gmail.com>
2022-09-07 14:36:53 -0700

    full creation of a table, adding rows along side

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- fully create a table named second_table
+-- second_table description:
+-- 	id INT
+-- 	name VARCHAR(256)
+-- 	score INT
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, "John", 10);
+INSERT INTO second_table(id, name, score) VALUES(2, "Alex", 3);
+INSERT INTO second_table(id, name, score) VALUES(3, "Bob", 14);
+INSERT INTO second_table(id, name, score) VALUES(4, "George", 8);
