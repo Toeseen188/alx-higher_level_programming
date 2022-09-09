cd95c8de6aab94549deedc9d3aaeeb108a6a6837
Hammed Tosin <hammedtosin188@gmail.com>
2022-09-09 14:01:45 -0700

    JOIN & COUNT

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Script that lists all shows contained in hbtn_0d_tvshows
+-- without a genre linked.
+SELECT ts.title, ts_g.genre_id
+FROM tv_shows AS ts
+	LEFT JOIN tv_show_genres AS ts_g
+	ON ts.id = ts_g.show_id
+WHERE ts_g.show_id IS NULL
