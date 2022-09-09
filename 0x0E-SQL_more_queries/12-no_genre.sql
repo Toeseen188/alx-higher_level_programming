ee3d6b2d966d69ebb7649f459252b8c1be472bee
Hammed Tosin <hammedtosin188@gmail.com>
2022-09-09 15:20:27 -0700

    fix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- Script that lists all shows contained in hbtn_0d_tvshows
 -- without a genre linked.
-SELECT ts.title, ts_g.genre_id
-FROM tv_shows AS ts
-	LEFT JOIN tv_show_genres AS ts_g
-	ON ts.id = ts_g.show_id
-WHERE ts_g.show_id IS NULL;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+	LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL;
