1c4af22dd222868d63f7236ff6f9988526319baa
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-05 20:34:05 -0700

    fic

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* convert argument to integer */
-const argTwo = parseInt(process.argv[2]);
+const argTwo = Number(process.argv[2]);
 if (argTwo) {
   console.log(`My Number: ${argTwo}`);
 } else {
