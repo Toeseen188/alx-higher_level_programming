5d6aae4a951e42602f84ee2c50a466e4b1888e76
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-05 19:02:03 -0700

    using while loop on array or list

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* print from an Array using loop */
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (array[i]) {
+  console.log(array[i]);
+  i++;
+}
