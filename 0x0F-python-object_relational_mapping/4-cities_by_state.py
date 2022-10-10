0493be9ec35c0cf513a0d4e03521c374ae5dbe84
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-10 06:40:55 -0700

    query from two tables using JOIN

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr//bin/python3
+# a script that lists all cities from the database hbtn_0e_4_usa
+# Usage: ./4-cities_by_state.py root root hbtn_0e_4_usa
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306,  user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = conn.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                    INNER JOIN states ON cities.state_id = states.id\
+                    ORDER BY cities.id")
+    for cities in cursor.fetchall():
+        print(cities)
+    cursor.close()
+    conn.close()
