9fb23c0cf84f77dd5fbefe1b4660d8e633d8a1ff
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-10 08:03:32 -0700

    module doc

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# a script that lists all states from the database hbtn_0e_0_usa
+""" a script that lists all states from the database hbtn_0e_0_usa
 # Usage: ./0-select_states.py root root hbtn_0e_0_usa
+"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
