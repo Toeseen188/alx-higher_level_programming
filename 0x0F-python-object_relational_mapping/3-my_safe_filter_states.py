7f5896808706cbac9161fc6810fb74d94c904572
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-10 06:03:17 -0700

    safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr//bin/python3
+# a script that lists all states from the database hbtn_0e_0_usa
+# search for state provided as argument
+# save from sql injection
+# Usage: ./2-filter-states.py root root hbtn_0e_0_usa 'states'
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306,  user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = conn.cursor()
+    search = sys.argv[4]
+    q = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(q)
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == search:
+            print(state)
+    cursor.close()
+    conn.close()
