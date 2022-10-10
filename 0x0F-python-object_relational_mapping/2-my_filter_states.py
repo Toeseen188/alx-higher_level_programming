543e0fce63565b5a98c7a39aad1d2edeff53c7f8
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-10 03:28:13 -0700

    filter using argument and format strings

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr//bin/python3
+# a script that lists all states from the database hbtn_0e_0_usa
+# search for state provided as argument
+# Usage: ./2-filter-states.py root root hbtn_0e_0_usa 'states'
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306,  user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = conn.cursor()
+    search = sys.argv[4]
+    q = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(search)
+    cursor.execute(q)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    conn.close()
