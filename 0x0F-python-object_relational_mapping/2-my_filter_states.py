23df52035181b6cd4550d62c2f114f649b2ae2d8
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-11 16:07:37 -0700

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = conn.cursor()
     search = sys.argv[4]
-    cursor.execute("""SELECT * FROM states WHERE name = "{}"
+    cursor.execute("""SELECT * FROM states WHERE BINARY name = "{}"
                     ORDER BY id ASC""".format(search))
     states = cursor.fetchall()
     for state in states:
