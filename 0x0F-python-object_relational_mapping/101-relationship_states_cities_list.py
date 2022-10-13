aa220dfb0e0bd5a5edc0ea111d57f94ae99052c2
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-12 18:31:22 -0700

    querying using relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Write a script that prints all City objects from
+the database hbtn_0e_14_usa
+#Usage:./101-relationship_states_cities_list root root hbtn_0e_101_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+from relationship_state import State
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            sys.argv[1], sys.argv[2],
+                            sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("     {}: {}".format(city.id, city.name))
+    session.close()
