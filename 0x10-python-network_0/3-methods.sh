2cca46573676093f4a57d23ab1af5cfedaaabbb9
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-20 17:48:09 -0700

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a request to webserve to display all the METHODS allowed
-curl -s -I "$1" | grep "Allow:" | cut -d " " -f 2-
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
