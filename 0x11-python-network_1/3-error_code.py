afabfba95f2b627af8204cf3e8e36b5413616229
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-21 18:19:09 -0700

    fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(request) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print('Error code: ', e.code)
+        print('Error code: {}'.format(e.code))
