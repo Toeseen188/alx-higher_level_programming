f4fcc280ac421573caee20e6b9ed3949061992b5
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-28 08:12:53 -0700

    fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
 
-    if r.status_code != requests.codes.ok:
-        print("Error code: {}".format(r.status_code))
-    else:
+    if r.status_code == requests.codes.ok:
         print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
