cad58a22c34038bfd3ce3dada7765d90179e1d29
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-28 08:16:06 -0700

    fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,6 @@
 """
 Write a Python script that takes in a URL, sends a request to the URL
 and displays the body of the response.
-
 -If the HTTP status code is greater than or equal to 400,
 print: Error code: followed by the value of the HTTP status code
 """
