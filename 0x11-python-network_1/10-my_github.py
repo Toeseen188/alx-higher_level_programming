b2837d12ff2521ec12b3d17e54ea95b48d9fe3ac
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-29 11:38:40 -0700

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     user = sys.argv[1]
     passw = sys.argv[2]
 
-    url = "https://api.github.com/user/{}".format(user)
+    url = "https://api.github.com/user/{}".format(passw)
 
     r = requests.get(url, auth=(user, passw))
 
